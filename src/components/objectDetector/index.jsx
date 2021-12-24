d29c8080d495a8e40ceb305d33fc0c7319fa28d1
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-24 11:47:36 +0000

    Working predictions of an image

--- src/components/objectDetector/index.jsx
+++ src/components/objectDetector/index.jsx
@@ -1,9 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import styled from 'styled-components'
 
 import '@tensorflow/tfjs-backend-cpu'
 import '@tensorflow/tfjs-backend-webgl'
-// import * as cocoSsd from '@tensorflow-models/coco-ssd'
+import * as cocoSsd from '@tensorflow-models/coco-ssd'
 
 const ObjectDetectorContainer = styled.div`
   display: flex;
@@ -22,9 +22,9 @@ const DetectorContainer = styled.div`
   position: relative;
 `
 
-// const TargetImg = styled.img`
-//   height: 100%;
-// `
+const TargetImg = styled.img`
+  height: 100%;
+`
 
 const HiddenFileInput = styled.input`
   display: none;
@@ -51,15 +51,50 @@ const SelectButton = styled.button`
 
 export function ObjectDetector(props) {
   const fileInputRef = useRef()
+  const [imgData, setImgData] = useState(null)
 
   const openFilePicker = () => {
     if (fileInputRef.current) fileInputRef.current.click()
   }
 
+  const detectObjectsOnImage = async (imageElement) => {
+    const model = await cocoSsd.load({})
+    const predictions = await model.detect(imageElement, 6)
+    console.log('predictions', predictions)
+  }
+
+  const readImage = (file) => {
+    return new Promise((rs, rj) => {
+      const fileReader = new FileReader()
+      fileReader.onload = () => rs(fileReader.result)
+      fileReader.onerror = () => rj(fileReader.error)
+      fileReader.readAsDataURL(file)
+    })
+  }
+
+  const onSelectImage = async (e) => {
+    const file = e.target.files[0]
+    const imgData = await readImage(file)
+    setImgData(imgData)
+
+    const imageElement = document.createElement('img')
+    imageElement.src = imgData
+
+    imageElement.onload = async () => {
+      await detectObjectsOnImage(imageElement)
+    }
+  }
+
   return (
     <ObjectDetectorContainer>
-      <DetectorContainer>Img</DetectorContainer>
-      <HiddenFileInput type="file" ref={fileInputRef} />
+      <DetectorContainer>
+        {imgData && <TargetImg src={imgData} alt="" />}
+      </DetectorContainer>
+      <HiddenFileInput
+        type="file"
+        ref={fileInputRef}
+        onChange={onSelectImage}
+      />
       <SelectButton onClick={openFilePicker}>Select Image</SelectButton>
     </ObjectDetectorContainer>
   )
