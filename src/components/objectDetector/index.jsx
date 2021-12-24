97d7804545334f4c6102db1d899cfb474912185c
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-24 12:45:03 +0000

    Adding sample images & resizing boundary box on recognized image

--- src/components/objectDetector/index.jsx
+++ src/components/objectDetector/index.jsx
@@ -49,17 +49,67 @@ const SelectButton = styled.button`
   }
 `
 
+const TargetBox = styled.div`
+  position: absolute;
+
+  left: ${({ x }) => x + 'px'};
+  top: ${({ y }) => y + 'px'};
+  width: ${({ width }) => width + 'px'};
+  height: ${({ height }) => height + 'px'};
+
+  border: 4px solid #1ac71a;
+  background-color: transparent;
+  z-index: 20;
+
+  &::before {
+    content: '${({ classType, score }) => `${classType} ${score.toFixed(1)}%`}';
+    color: #1ac71a;
+    font-weight: 500;
+    font-size: 17px;
+    position: absolute;
+    top: -1.5em;
+    left: -5px;
+  }
+`
+
 export function ObjectDetector(props) {
   const fileInputRef = useRef()
+  const imageRef = useRef()
   const [imgData, setImgData] = useState(null)
+  const [predictions, setPredictions] = useState([])
+
+  const isEmptyPredictions = !predictions || predictions.length === 0
 
   const openFilePicker = () => {
     if (fileInputRef.current) fileInputRef.current.click()
   }
 
-  const detectObjectsOnImage = async (imageElement) => {
+  const normalizePredictions = (predictions, imgSize, imgRef) => {
+    if (!predictions || !imgSize || !imageRef) return predictions || []
+    return predictions.map((prediction) => {
+      const { bbox } = prediction
+      const oldX = bbox[0]
+      const oldY = bbox[1]
+      const oldWidth = bbox[2]
+      const oldHeight = bbox[3]
+
+      const imgWidth = imageRef.current.width
+      const imgHeight = imageRef.current.height
+
+      const x = (oldX * imgWidth) / imgSize.width
+      const y = (oldY * imgHeight) / imgSize.height
+      const width = (oldWidth * imgWidth) / imgSize.width
+      const height = (oldHeight * imgHeight) / imgSize.height
+
+      return { ...prediction, bbox: [x, y, width, height] }
+    })
+  }
+
+  const detectObjectsOnImage = async (imageElement, imgSize) => {
     const model = await cocoSsd.load({})
     const predictions = await model.detect(imageElement, 6)
+    const normalizedPredictions = normalizePredictions(predictions, imgSize)
+    setPredictions(normalizedPredictions)
     console.log('predictions', predictions)
   }
 
@@ -81,14 +131,27 @@ export function ObjectDetector(props) {
     imageElement.src = imgData
 
     imageElement.onload = async () => {
-      await detectObjectsOnImage(imageElement)
+      const imgSize = { width: imageElement.width, height: imageElement.height }
+      await detectObjectsOnImage(imageElement, imgSize)
     }
   }
 
   return (
     <ObjectDetectorContainer>
       <DetectorContainer>
-        {imgData && <TargetImg src={imgData} alt="" />}
+        {imgData && <TargetImg src={imgData} ref={imageRef} alt="" />}
+        {!isEmptyPredictions &&
+          predictions.map((prediction, idx) => (
+            <TargetBox
+              key={idx}
+              x={prediction.bbox[0]}
+              y={prediction.bbox[1]}
+              width={prediction.bbox[2]}
+              height={prediction.bbox[3]}
+              classType={prediction.class}
+              score={prediction.score * 100}
+            />
+          ))}
       </DetectorContainer>
       <HiddenFileInput
         type="file"
